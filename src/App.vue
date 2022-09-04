a1a9e51b0bfb85c800c609328bed705210b94ab6
ywt <569845499@qq.com>
2022-09-05 02:12:11 +0800

    feat: tabbar dynamic data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <h2>App</h2>
   <router-view></router-view>
-  <router-link to="/">首页</router-link>
-  <router-link to="/favor">收藏</router-link>
-  <router-link to="/order">订单</router-link>
-  <router-link to="/message">消息</router-link>
+
+  <tab-bar></tab-bar>
+
 </template>
 
 <script setup>
+import tabBar from './components/tab-bar/tab-bar.vue';
 
 </script>
 
---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="tab-bar">
+    <template v-for="(item, index) in tabbarData" :key="index">
+      <router-link :to="item.path" class="tab-bar-item">
+        <img src="../../assets/image/tabbar/tab_message.png" alt="order">
+        <span class="text">{{ item.text }}</span>
+      </router-link>
+    </template>
+  </div>
+</template>
+
+<script setup>
+
+const tabbarData = [
+  {
+    text: '首页',
+    img: '@/assets/image/tabbar/tab_home.png',
+    imgActive: '@/assets/image/tabbar/tab_home_active.png',
+    path: '/home'
+  },
+  {
+    text: '收藏',
+    img: '@/assets/image/tabbar/tab_favor.png',
+    imgActive: '@/assets/image/tabbar/tab_favor_active.png',
+    path: '/favor'
+  },
+  {
+    text: '订单',
+    img: '@/assets/image/tabbar/tab_order.png',
+    imgActive: '@/assets/image/tabbar/tab_order_active.png',
+    path: '/order'
+  },
+  {
+    text: '消息',
+    img: '@/assets/image/tabbar/tab_message.png',
+    imgActive: '@/assets/image/tabbar/tab_message.png',
+    path: '/message'
+  },
+]
+
+</script>
+
+<style lang="less" scoped>
+.tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+
+  border-top: 1px solid #eee;
+
+  .tab-bar-item {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      width: 36px;
+    }
+
+    .text {
+      font-size: 12px;
+      margin-top: 2px;
+    }
+  }
+}
+</style>
