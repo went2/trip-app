6d947bc42d6564008744bb01ce37af25d868e1f3
wentao <569845499@qq.com>
2022-09-06 17:08:09 +0800

    feat: add tabs in city view

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="city top-page">
-    <h2>city</h2>
+    <form action="/">
+      <van-search
+        v-model="seachValue"
+        shape="round"
+        show-action
+        placeholder="城市/区域/位置"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
+
+    <van-tabs v-model:active="tabActive" color="#ff9854">
+      <van-tab title="国内·港澳台" name="a"></van-tab>
+      <van-tab title="海外" name="b"></van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script setup>
+import {ref} from 'vue';
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+
+// 搜索
+const seachValue = ref("");
+const onSearch = () => {
+
+};
+
+const onCancel = () => {
+  router.back();
+};
+
+// tab切换
+const tabActive = ref("a");
+
 
 </script>
 
 <style lang="less" scoped>
+
+
 
 </style>
