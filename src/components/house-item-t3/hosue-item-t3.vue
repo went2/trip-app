2d143125290336722290cb4267f5587e70d92566
ywt <569845499@qq.com>
2022-09-10 15:42:32 +0800

    feat: add a another style of house card

--- src/components/house-item-t3/hosue-item-t3.vue
+++ src/components/house-item-t3/hosue-item-t3.vue
@@ -13,15 +13,14 @@
         <div class="summary">{{ itemData.summaryText }}</div>
         
         <div class="price">
-          <div class="rate">
-            <van-rate
-              :model-value="Number(itemData.commentScore)" 
-              readonly allow-half
-              color="#fff" void-color="#fff"
-              size="12px"
-            />
-          </div>
           <div class="new">￥{{ itemData.finalPrice }}</div>
+          <div class="old">￥{{ itemData.productPrice }}</div>
+          <div
+            class="discount"
+            :style="discountStyle"
+          >
+            {{ itemData.priceTipBadge.text }}
+          </div>
         </div>
       </div>
     </div>
@@ -29,14 +28,22 @@
 </template>
 
 <script setup>
+import {ref, computed, reactive} from 'vue';
 
-defineProps({
+const props = defineProps({
   itemData: {
     type: Object,
     default: () => ({})
   },
-})
+});
 
+const startColor = computed(() => props.itemData.priceTipBadge.gradient.startColor);
+const endColor = computed(() => props.itemData.priceTipBadge.gradient.endColor);
+
+const discountStyle = reactive({
+  color: props.itemData.priceTipBadge.color,
+  backgroundImage: `linear-gradient(to right, ${startColor.value}, ${'#' + endColor.value})`
+});
 
 </script>
 
@@ -44,7 +51,7 @@ defineProps({
 .house-item {
   width: 50%;
   padding: 5px;
-  border: 1px solid #000;
+  // border: 1px solid #000;
   
   .item-inner {
     position: relative;
@@ -68,13 +75,14 @@ defineProps({
         }
         .text {
           margin-left: 5px;
-          color: grey;
+          color: rgb(60, 60, 60);
         }
-      } 
+      }
       .summary {
         font-size: 12px;
       }
       .name {
+        line-height: 1.2;
         margin: 8px 0;
         overflow: hidden;
         text-overflow: ellipsis;
@@ -83,12 +91,27 @@ defineProps({
         -webkit-box-orient: vertical;
       }
 
-      .price {
-        display: flex;
-        justify-content: space-between;
-        .rate {
+    }
 
-        }
+    .price {
+      margin-top: 8px;
+      display: flex;
+      align-items: center;
+      .new {
+        color: #ff9645;
+      }
+
+      .old {
+        margin-left: 5px;
+        color: #999;
+        text-decoration:line-through;
+        font-size: 12px;
+      }
+      .discount {
+        margin-left: 5px;
+        font-size: 12px;
+        padding: 1px 8px;
+        border-radius: 10px;
       }
     }
   }
