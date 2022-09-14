a396d570ab8ba16dce7f8389126a5cfa17b59af5
ywt <569845499@qq.com>
2022-09-14 17:06:01 +0800

    feat: custom vant swipe indicator, good challenge!

--- src/views/detail/components/detail-swipe.vue
+++ src/views/detail/components/detail-swipe.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="detail-swipe">
-    <van-swipe class="list" :autoplay="3000" indicator-color="white">
+    <van-swipe class="list" :autoplay="3000">
       <template v-for="(item, index) in swipeData" :key="index">
         <van-swipe-item>
           <img :src="item.url" alt="">
         </van-swipe-item>
+      </template>
+
+      <template #indicator="{ active, total }">
+        <div class="indicator">
+          <template v-for="(value, key, index) in swipeGroup" :key="key">
+            <span
+              class="item"
+              :class="{ active: swipeData[active]?.enumPictureCategory == key }"
+            >
+              <span class="text">{{ formatTitle(value[0].title) }}</span>
+              <span class="count" v-if="swipeData[active]?.enumPictureCategory == key">
+                {{ getCategoryIndex(swipeData[active]) }}/{{ value.length }}
+              </span>
+            </span>
+          </template>
+        </div>
       </template>
     </van-swipe>
   </div>
@@ -17,7 +33,35 @@ const props = defineProps({
     type: Array,
     default: () => []
   }
-})
+});
+
+/**
+ * 处理数据
+ * result: swipeGroup: { "2": [obj1, obj2], "4": [obj3, obj4] }
+ */
+const swipeGroup = {};
+for(const item of props.swipeData) {
+  let groupValue = swipeGroup[item.enumPictureCategory];
+  if(!groupValue) {
+    swipeGroup[item.enumPictureCategory] = [];
+  }
+  swipeGroup[item.enumPictureCategory].push(item);
+}
+
+const titleReg = /【(.*?)】/i;
+const formatTitle = (title) => {
+  const result = titleReg.exec(title);
+  return result[1];
+}
+
+const getCategoryIndex = (activeItem) => {
+  // find target array in swipeGroup
+  const valueArr = swipeGroup[activeItem.enumPictureCategory];
+
+  // find index of target object
+  const index = valueArr.findIndex(item => item === activeItem);
+  return index + 1;
+}
 
 </script>
 
@@ -27,6 +71,31 @@ const props = defineProps({
     img {
       width: 100%;
     }
+
+    .indicator {
+        position: absolute;
+        right: 5px;
+        bottom: 5px;
+        padding: 4px 8px;
+        border-radius: 12px;
+        display: flex;
+        font-size: 12px;
+        color: #fff;
+        background: rgba(0, 0, 0, 0.8);
+
+        .item {
+          margin: 0 4px;
+          &.active {
+            padding: 0px 4px;
+            background-color: #fff;
+            border-radius: 6px;
+            color: #555;
+          }
+          .count {
+            margin-left: 3px;
+          }
+        }
+      }
   }
 }
 </style>
