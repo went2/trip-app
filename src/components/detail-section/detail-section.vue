2daeb5095c57c883f4b4a3635e444ce938a8a418
wentao <569845499@qq.com>
2022-09-15 16:49:02 +0800

    feat: add common detail section component

---
+++ src/components/detail-section/detail-section.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="section">
+    <div class="header">
+      <div class="title">{{ title }}</div>
+    </div>
+    <div class="content">
+      <slot>
+        <h2>默认内容</h2>
+      </slot> 
+    </div>
+
+    <div class="footer">
+      <div class="more">
+        <span>{{ moreText }}</span>
+        <van-icon name="arrow" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: '默认标题'
+  },
+  moreText:{
+    type: String,
+    default: '查看更多'
+  }
+})
+
+</script>
+
+<style lang="less" scoped>
+.section {
+  margin: 5px 0;
+  border-top: 5px solid #ddd;
+  .footer {
+    display: flex;
+    justify-content: flex-end;
+    .more {
+      // position: absolute;
+      color: var(--primary-color);
+    }
+  }
+}
+
+
+</style>
