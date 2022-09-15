a898f83d29572a92f7f97a6e221cd961f7f115f0
wentao <569845499@qq.com>
2022-09-15 15:36:40 +0800

    feat: finish top info section

--- src/views/detail/components/detail_02-info.vue
+++ src/views/detail/components/detail_02-info.vue
@@ -12,6 +12,28 @@
         </span>
       </template>  
     </div>
+
+    <div class="bar comment">
+      <div class="left">
+        <span class="overall">{{ topInfo.commentBrief.overall }}</span>
+        <span class="score-title">{{ topInfo.commentBrief.scoreTitle }}</span>
+        <span class="comment-brief">"{{ topInfo.commentBrief.commentBrief }}"</span>
+      </div>
+      <div class="right">
+        <span>{{ topInfo.commentBrief.totalCount }}</span>条评论
+        <van-icon name="arrow" />
+      </div>
+    </div>
+
+    <div class="bar position">
+      <div class="left">
+        {{ topInfo.nearByPosition.address }}
+      </div>
+      <div class="right">
+        <span>地图·周边</span>
+        <van-icon name="arrow" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,6 +67,45 @@ defineProps({
       font-size: 13px;
       padding: 2px;
       margin: 0 3px;
+    }
+  }
+
+  .bar {
+    margin-top: 11px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #f1f4f5;
+    padding: 10px;
+    border-radius: 8px;
+
+    .right {
+      color: var(--primary-color);
+    }
+  }
+
+  .comment {
+    .left {
+      .overall {
+        font-size: 18px;
+        font-weight: 500;
+      }
+      .score-title {
+        margin-left: 4px;
+        font-size: 13px;
+        font-weight: 500;
+      }
+      .comment-brief {
+        margin-left: 4px;
+        font-size: 13px;
+        color: #999;
+      }
+    }
+  }
+
+  .position {
+    .left {
+      font-weight: 500;
     }
   }
 }
